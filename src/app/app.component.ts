a75bc4435b7e6d8ed040666d81ad6ea546be0333
Sarah López <a3521110542@alumno.uttehuacan.edu.mx>
2023-03-20 15:15:23 -0600

    subiendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from './components/models/product.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+  showImg=true;
+
+  products:Product[]=[
+    {
+
+        id:'1',
+        name:'El mejor juguete',
+        image:'../../../assets/images/my-hero-academia-quirks-1557833813.jpg',
+        price:565.00
+    },
+    {
+
+      id:'2',
+      name:'Bicicleta casi nueva',
+      image:'../../../assets/images/my-hero-academia-quirks-1557833813.jpg',
+      price:1250.00
+  },
+  {
+
+    id:'3',
+    name:'Colección de albumes',
+    image:'../../../assets/images/my-hero-academia-quirks-1557833813.jpg',
+    price:1100.00
+  },
+  {
+
+    id:'4',
+    name:'Serie de novelas',
+    image:'../../../assets/images/my-hero-academia-quirks-1557833813.jpg',
+    price:800.00
+  },
+  {
+
+    id:'5',
+    name:'Lentes para disfrutar',
+    image:'../../../assets/images/my-hero-academia-quirks-1557833813.jpg',
+    price:100.00
+  },
+  {
+
+    id:'6',
+    name:'casa de muñecas',
+    image:'../../../assets/images/my-hero-academia-quirks-1557833813.jpg',
+    price:2300.00
+  },
+
+
+  ];
+
+  onLoaded(img:string){
+    console.log('Load padre', img);
+  }
+  toggleImg(){
+    this.showImg=!this.showImg;
+  }
 }
